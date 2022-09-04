d54074eeb9b91679948b3f1122a9d33642af2d29
Howard Wu <40033067+Howard20181@users.noreply.github.com>
2022-09-05 01:29:48 +0800

    Implement network connectivity check by ping
    This is helpful when using mwan3

--- autoauth.sh
+++ autoauth.sh
@@ -8,6 +8,19 @@ BasePath=$(
     pwd
 )
 # DEBUG=1
+
+# ping host or curl generate_204
+# net_track_method="ping" # ping or curl
+
+# for ping
+PING_TRACK_IP="10.7.11.254"
+IP_FAMILY="ipv4"
+INTERFACE="wan"
+DEVICE="wan"
+
+# for curl
+NET_CHECK_CURL_URL="https://connect.rom.miui.com/generate_204"
+
 CONF="$BasePath"/user.conf
 BaseName=$(basename "$0")
 LOG() {
@@ -73,37 +86,106 @@ get_json_value() {
 
 SHOULD_STOP() {
     local TIME_STOP1 TIME_STOP2 TIME_CUR
-    TIME_STOP1=$(date -d "$(date '+%Y-%m-%d 23:59:59')" +%s)
+    TIME_STOP1=$(date -d "$(date '+%Y-%m-%d 23:40:00')" +%s)
     TIME_STOP2=$(date -d "$(date '+%Y-%m-%d 07:00:00')" +%s)
     TIME_CUR=$(date +%s)
     if [ "$portServIncludeFailedCode" = "63027" ]; then
         if [ "$TIME_CUR" -gt "$TIME_STOP1" ] || [ "$TIME_CUR" -lt "$TIME_STOP2" ]; then
+            if [ $(( TIME_CUR - TIME_STOP1 )) -lt 0 ];then
+                SLEEP_TIME=$(( TIME_STOP2 - TIME_CUR ))
+            else
+                SLEEP_TIME=$(( TIME_STOP2 + 86400 - TIME_CUR ))
+            fi
             return 0
         else
             SLEEP_TIME="1"
             return 1
         fi
     else
-        SLEEP_TIME="1"
         return 1
     fi
 }
 
+# ported from mwan3track
+WRAP() {
+	# shellcheck disable=SC2048
+	FAMILY=$IP_FAMILY DEVICE=$DEVICE $*
+}
+ping_test_host() {
+	if [ "$IP_FAMILY" = "ipv6" ]; then
+		echo "::1"
+	else
+		echo "127.0.0.1"
+	fi
+}
+get_ping_command() {
+	if [ -x "/usr/bin/ping" ] && /usr/bin/ping -${IP_FAMILY#ipv} -c1 -q "$(ping_test_host)" &>/dev/null; then
+		# -4 option added in iputils c3e68ac6 so need to check if we can use it
+		# or if we must use ping and ping6
+		echo "/usr/bin/ping -${IP_FAMILY#ipv}"
+	elif [ "$IP_FAMILY" = "ipv6" ] && [ -x "/usr/bin/ping6" ]; then
+		echo "/usr/bin/ping6"
+	elif [ "$IP_FAMILY" = "ipv4" ] && [ -x "/usr/bin/ping" ]; then
+		echo "/usr/bin/ping"
+	elif [ -x "/bin/ping" ]; then
+		echo "/bin/ping -${IP_FAMILY#ipv}"
+	else
+		return 1
+	fi
+}
+validate_track_method() {
+	case "$1" in
+		ping)
+			if ! PING=$(get_ping_command); then
+				LOG W "Missing ping. Please enable BUSYBOX_DEFAULT_PING and recompile busybox or install iputils-ping package."
+				return 1
+			fi
+			;;
+		curl)
+			command -v curl 1>/dev/null 2>&1 || {
+				LOG W "Missing curl. Please install curl package."
+				return 1
+			}
+			;;
+	esac
+}
+
 NET_AVAILABLE() {
-    if [ "$(curl -sI -w "%{http_code}" -o /dev/null connect.rom.miui.com/generate_204)" = "204" ]; then
-        RECONN_COUNT="0"
-        return 0
-    else
-        return 1
-    fi
+    case "$net_track_method" in
+        ping)
+			WRAP "$PING" -n -c 1 -q $PING_TRACK_IP &> /dev/null &
+            wait $!
+            result=$?
+            if [ $result -eq 0 ]; then
+                RECONN_COUNT="0"
+                return 0
+            else
+                return 1
+            fi
+			;;
+        curl)
+            if [ "$(curl -sI -w "%{http_code}" -o /dev/null $NET_CHECK_CURL_URL)" = "204" ]; then
+                RECONN_COUNT="0"
+                return 0
+            else
+                return 1
+            fi
+			;;
+    esac
 }
 
 restart_auth() {
     RECONN_COUNT=$(( RECONN_COUNT + 1 ))
-    SLEEP_TIME=$(( SLEEP_TIME * RECONN_COUNT ))
-    LOG N "Wait ${SLEEP_TIME}s"
-    sleep "$SLEEP_TIME"
-    LOG N "Reconnecting: $RECONN_COUNT TIME"
+    if SHOULD_STOP; then
+        LOG N "sleep ${SLEEP_TIME}s"
+        sleep $SLEEP_TIME
+    else
+        SLEEP_TIME=$(( SLEEP_TIME * RECONN_COUNT ))
+        LOG N "Wait ${SLEEP_TIME}s"
+        sleep "$SLEEP_TIME"
+        LOG N "Reconnecting: $RECONN_COUNT TIME"
+    fi
+    LOG I "continue"
     start_auth
 }
 
@@ -149,11 +231,6 @@ start_auth() {
                             exit 1
                         fi
                     done
-                    while SHOULD_STOP; do
-                        LOG D "sleep ${SLEEP_TIME}s"
-                        sleep $SLEEP_TIME
-                    done
-                    LOG I "continue"
                     restart_auth
                 fi
             fi
@@ -163,15 +240,19 @@ start_auth() {
         fi
     fi
 }
+validate_track_method "$net_track_method" || {
+		net_track_method=curl
+		if validate_track_method $net_track_method; then
+			LOG W "Using curl to track interface $INTERFACE avaliability"
+		else
+			LOG E "No track method avaliable"
+			exit 1
+		fi
+	}
 start_auth
 
 while true; do
     if ! (NET_AVAILABLE); then
-        while SHOULD_STOP; do
-            LOG D "sleep ${SLEEP_TIME}s"
-            sleep $SLEEP_TIME
-        done
-        LOG I "continue"
         restart_auth
     fi
     sleep $SLEEP_TIME
